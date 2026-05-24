e44e59d693a009c5e997325a00e66a5729674c21
marufbytes <marufahammed215@gmail.com>
2026-05-24 21:16:40 +0600

    Login page design

---
+++ script/login.js
@@ -0,0 +1,27 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    //1. get the mobile number input
+    const numberInput = document.getElementById('input-number');
+    const contactNumber = numberInput.value;
+    console.log(contactNumber)
+
+    //2. get the pin
+    const inputPin = document.getElementById('input-pin');
+    const pin = inputPin.value;
+    console.log(pin)
+
+    //3. match pin and mobile number
+    if (contactNumber == '01234567890' && pin == '1234') {
+        //3-1 true::>> alert homepage
+        alert('Login successful')
+        // window.location.replace("/home.html")
+        window.location.assign("/home.html")
+
+    }
+    //3-1 true::>> alert return
+    else{
+        alert("Login failed")
+        return;
+    }
+
+
+})
