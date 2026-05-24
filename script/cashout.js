1783684f5d5ab73d38936bdcfcbe1032fa41a99d
marufbytes <marufahammed215@gmail.com>
2026-05-24 22:19:37 +0600

    Cashout UI Design

---
+++ script/cashout.js
@@ -0,0 +1,7 @@
+document.getElementById('cashout-btn').addEventListener('click', function(){
+    //1. get the agent number & validate
+    //2. get the amount, validate, convert to Number
+    //3. get current balance, validate, convert to Number
+    //4. claculate new Balance, 
+    //5. get the pin and verify(true, false, alert)
+})
